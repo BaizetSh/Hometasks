5dafdc5c939396ebcd3f9b88ded3743470672680
BaizetSh <bych7777@gmail.com>
2022-10-11 12:23:53 +0300

    Практика №3

---
+++ Практическая №3/untitled3/src/main/kotlin/Main.kt
@@ -0,0 +1,84 @@
+import java.io.StringBufferInputStream
+import java.util.*
+import kotlin.math.roundToInt
+import kotlin.math.sqrt
+
+fun main(args: Array<String>) {
+    /*
+     // Задача №1
+      val myAge:Int=17
+      val isTeenager:Boolean=13<=myAge&&myAge<=19
+      println(isTeenager)
+      // Задача №2
+      val myAge:Int=17
+      val theirAge:Int=30
+      val bothTeenagers:Boolean=13<=myAge&&myAge<=19&&13<=theirAge&&theirAge<=19
+      println(bothTeenagers)
+      // Задача №3
+      val reader:String="Baizet"
+      val author:String="Richard Lucas"
+      val authorIsReader=reader == author
+      println(authorIsReader)
+      // Задача №4
+      val reader:String="Baizet"
+      val author:String="Richard Lucas"
+      val readerBeforeAuthor=author > reader
+      println(readerBeforeAuthor)
+      // Задача №5
+      val myAge2:Int=18
+      if (13<=myAge2&&myAge2<=19)
+      {
+          println("Подросток")
+      }
+      else
+          println("Не подросток")
+      // Задача №6
+      val myAge2:Int=18
+      val answer=if(13<=myAge2&&myAge2<=19)
+       println("Подросток")
+      else
+       println("Не подросток")
+      // Задача №7
+      var counter:Int=0
+      while(counter < 10)
+      {
+      var x:Int=counter
+      counter++
+      println(x)
+      }
+      // Задача №8
+      var counter:Int=0
+      var roll:Int=0
+      do {
+          roll=Random().nextInt(6)
+          counter++
+          println("После $counter бросков, roll равен $roll")
+      } while(counter < 0)
+      // Задача №9
+      val range=Random().nextInt(10)
+      val count=10
+      var sum:Int=0
+      for (range in range..count) {
+          println("range =$range")
+          sum=range*r5ange
+          println(sum)
+      }
+      // Задача №10
+      val range=Random().nextInt(10)
+      val count=10
+      var sum:Double=0.0
+      for (range in range..count) {
+          println("range =$range")
+          sum=sqrt(range.toDouble())
+          println(String.format("%.3f",sum))
+      }
+      // Задача №11
+      var sum:Int=0
+      for(row in 1 until 8 step 2) {
+          for(column in 0 until 8) {
+              sum+=row*column
+          }
+      }
+          println(sum)
+     */
+}
